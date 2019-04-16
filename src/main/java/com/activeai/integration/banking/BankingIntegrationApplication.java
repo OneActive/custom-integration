85ae754b3937f414bf9c3fd0b29d47cb6d054e3c
Puneet Khanna <puneet.khanna@active.ai>
2019-04-16 12:36:13 +0530

    Fixed war build related issues

--- src/main/java/com/activeai/integration/banking/BankingIntegrationApplication.java
+++ src/main/java/com/activeai/integration/banking/BankingIntegrationApplication.java
@@ -1,13 +1,26 @@
 package com.activeai.integration.banking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class BankingIntegrationApplication  {
+@EnableAutoConfiguration
+@ImportResource("classpath:applicationContext.xml")
+@ComponentScan("com.activeai.integration.banking")
+public class BankingIntegrationApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(BankingIntegrationApplication.class, args);
+  }
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(BankingIntegrationApplication.class);
   }
   
 }
