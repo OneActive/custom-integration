1607c45073eb6be2c55941c2dfb8fb9e59c5c9c7
shivam74662 <shivam.thombre@active.ai>
2019-05-03 16:36:46 +0530

    Added Registered Biller API

--- src/main/java/com/activeai/integration/banking/controller/BillPaymentApiController.java
+++ src/main/java/com/activeai/integration/banking/controller/BillPaymentApiController.java
@@ -1,33 +1,31 @@
 package com.activeai.integration.banking.controller;
 
-import java.io.IOException;
-
-import javax.validation.Valid;
-
+import com.activeai.integration.banking.domain.response.BillerCategoriesResponse;
+import com.activeai.integration.banking.model.BillerResponse;
+import com.activeai.integration.banking.services.BillPaymentService;
+import com.activeai.integration.banking.utils.ApplicationLogger;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.activeai.integration.banking.domain.response.BillerCategoriesResponse;
-import com.activeai.integration.banking.model.BillerResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @Api(value = "BillPayment Related APIs", description = "Shows API Documentation Regards Billpayment APIs")
 @RestController
 public class BillPaymentApiController {
 
-  private static final Logger logger = LoggerFactory.getLogger(BillPaymentApiController.class);
-
   @Autowired
   private ObjectMapper objectMapper;
+
+  @Autowired
+  private BillPaymentService billpaymentService;
 
   @RequestMapping(value = "/billers/categories", produces = {"application/json"}, method = RequestMethod.GET)
   public ResponseEntity<BillerCategoriesResponse> getBillerCategoriesList() {
@@ -37,7 +35,7 @@ public class BillPaymentApiController {
           "{  \"result\" : {    \"messageCode\" : \"messageCode\",    \"message\" : \"message\",    \"status\" : 0  },  \"billerCategories\" : [ {    \"categoryName\" : \"categoryName\",    \"categoryId\" : \"categoryId\"  }, {    \"categoryName\" : \"categoryName\",    \"categoryId\" : \"categoryId\"  } ]}",
           BillerCategoriesResponse.class), HttpStatus.OK);
     } catch (IOException e) {
-      logger.error("Couldn't serialize response for content type application/json", e);
+      ApplicationLogger.logError("Couldn't serialize response for content type application/json", e);
       response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
     return response;
@@ -51,25 +49,16 @@ public class BillPaymentApiController {
           "{  \"result\" : {    \"messageCode\" : \"messageCode\",    \"message\" : \"message\",    \"status\" : 0  },  \"billers\" : [ {    \"billerCategory\" : \"billerCategory\",    \"billerId\" : \"billerId\",    \"allowPartialPay\" : true,    \"billerName\" : \"billerName\"  }, {    \"billerCategory\" : \"billerCategory\",    \"billerId\" : \"billerId\",    \"allowPartialPay\" : true,    \"billerName\" : \"billerName\"  } ]}",
           BillerResponse.class), HttpStatus.OK);
     } catch (IOException e) {
-      logger.error("Couldn't serialize response for content type application/json", e);
+      ApplicationLogger.logError("Couldn't serialize response for content type application/json", e);
       response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
     return response;
   }
 
+  @ApiOperation(value = "Returns list of Registered Billers based on customer ID")
   @RequestMapping(value = "/{customerId}/billers", produces = {"application/json"}, method = RequestMethod.GET)
-  public ResponseEntity<BillerResponse> getBillerListForCustomer(@RequestParam(value = "categoryId", required = false) Integer categoryId,
-      @PathVariable(value = "customerId", required = true) String customerId) {
-    ResponseEntity<BillerResponse> response = null;
-    try {
-      response = new ResponseEntity<>(objectMapper.readValue(
-          "{  \"result\" : {    \"messageCode\" : \"messageCode\",    \"message\" : \"message\",    \"status\" : 0  },  \"billers\" : [ {    \"billerCategory\" : \"billerCategory\",    \"billerId\" : \"billerId\",    \"allowPartialPay\" : true,    \"billerName\" : \"billerName\"  }, {    \"billerCategory\" : \"billerCategory\",    \"billerId\" : \"billerId\",    \"allowPartialPay\" : true,    \"billerName\" : \"billerName\"  } ]}",
-          BillerResponse.class), HttpStatus.OK);
-    } catch (IOException e) {
-      logger.error("Couldn't serialize response for content type application/json", e);
-      response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-    return response;
+  public ResponseEntity<BillerResponse> getRegisteredBillerListForCustomer(@RequestParam(value = "categoryId", required = false) Integer categoryId,@PathVariable(name = "customerId", required = true) String customerId) {
+    ApplicationLogger.logInfo("Entering getRegisteredBillers API");
+    return billpaymentService.getRegisteredBillerResponseEntity(customerId);
   }
-
 }
