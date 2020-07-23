4317b2e6f1c1050b57d2a073cbdf2026ca4c108e
Manjunath <manjunath.h@active.ai>
2020-07-23 20:47:45 +0530

    Added open api's to update payees,transactions response

--- src/main/java/com/activeai/integration/data/controller/CoreBankingController.java
+++ src/main/java/com/activeai/integration/data/controller/CoreBankingController.java
@@ -1,11 +1,13 @@
 package com.activeai.integration.data.controller;
 
+import com.activeai.integration.banking.domain.response.AccountTransactionsResponse;
+import com.activeai.integration.banking.domain.response.CardTransactionsResponse;
+import com.activeai.integration.banking.domain.response.PayeesResponse;
+import com.activeai.integration.data.model.CoreBankingModel;
+import com.activeai.integration.data.service.CoreBankingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.activeai.integration.banking.utils.ApplicationLogger;
 import com.activeai.integration.data.service.GenericService;
@@ -13,14 +15,20 @@ import com.activeai.integration.data.service.GenericService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import java.util.Map;
+
 @RestController("coreBankingController")
-@RequestMapping("/reset")
+@RequestMapping("data")
 @Api(value = "Handle Redis Data", description = "To Reset Redis Data as per the requirement")
 public class CoreBankingController {
 
   @Autowired
   private GenericService genericService;
-  
+
+  @Autowired
+  private CoreBankingService coreBankingService;
+
+
   /*
   Return health check
    */
@@ -33,33 +41,97 @@ public class CoreBankingController {
   }
 
   @ApiOperation(value = "Clear Redis Completely")
-  @RequestMapping(value = "/", method = RequestMethod.GET)
+  @RequestMapping(value = "/reset", method = RequestMethod.GET)
   public String resetCache() {
     return genericService.resetAll();
   }
 
   @ApiOperation(value = "Delete Customer specific information from redis")
-  @RequestMapping(value = "/{customerId}", method = RequestMethod.GET)
+  @RequestMapping(value = "/reset/{customerId}", method = RequestMethod.GET)
   public String resetCacheByCustomerId(@PathVariable(value = "customerId") String customerId) {
     return genericService.resetCacheByCustomerId(customerId);
   }
 
   @ApiOperation(value = "Delete Credit cards from redis")
-  @RequestMapping(value = "/{customerId}/creditcards", method = RequestMethod.GET)
+  @RequestMapping(value = "/reset/{customerId}/creditcards", method = RequestMethod.GET)
   public String resetCreditCards(@PathVariable(value = "customerId") String customerId) {
     return genericService.resetCreditCards(customerId);
   }
 
   @ApiOperation(value = "Delete Card transactions from redis")
-  @RequestMapping(value = "/{customerId}/cards/transactions", method = RequestMethod.GET)
+  @RequestMapping(value = "/reset/{customerId}/cards/transactions", method = RequestMethod.GET)
   public String resetCardTransactions(@PathVariable(value = "customerId") String customerId) {
     return genericService.resetCardsTransactions(customerId);
   }
 
   @ApiOperation(value = "Delete accounts transaction from redis")
-  @RequestMapping(value = "/{customerId}/accounts/transactions", method = RequestMethod.GET)
+  @RequestMapping(value = "/reset/{customerId}/accounts/transactions", method = RequestMethod.GET)
   public String resetAccountsTransactions(@PathVariable(value = "customerId") String customerId) {
     return genericService.resetAccountsTransactions(customerId);
+  }
+
+  /**
+   * Update Payee Response for specific customers
+   *
+   * @param payeesResponse
+   * @param customerId
+   * @return
+   */
+  @ApiOperation(value = "Update Payee Response for specific users")
+  @RequestMapping(value = "/init/{customerId}/payee", consumes = {"application/json"}, method = RequestMethod.POST)
+  public ResponseEntity<String> resetPayeeResponse(@RequestBody PayeesResponse payeesResponse,
+      @PathVariable(name = "customerId", required = true) String customerId) {
+    ApplicationLogger.logInfo("Updating payee response...");
+    CoreBankingModel coreBankingModel = coreBankingService.getCoreBankingModel(customerId);
+    coreBankingModel.setPayeesResponse(payeesResponse);
+    coreBankingService.saveCoreBankingModel(coreBankingModel);
+    String response = "Re-initialised Payee response successfully!";
+    return ResponseEntity.ok(response);
+  }
+
+  /**
+   * Reset Account related Transactions for specific customer
+   *
+   * @param accountTransactionsResponseMap
+   * @param customerId
+   * @param accountId
+   * @return
+   */
+  @ApiOperation(value = "Update accounts transactions Response for specific users")
+  @RequestMapping(value = "/init/{customerId}/accounts/transactions", consumes = {
+      "application/json"}, method = RequestMethod.POST)
+  public ResponseEntity<String> resetAccountTransactionsResponse(
+      @RequestBody Map<String, AccountTransactionsResponse> accountTransactionsResponseMap,
+      @PathVariable(name = "customerId", required = true) String customerId,
+      @PathVariable(name = "accountId", required = true) String accountId) {
+    ApplicationLogger.logInfo("Updating account transactions response...");
+    CoreBankingModel coreBankingModel = coreBankingService.getCoreBankingModel(customerId);
+    coreBankingModel.setAccountTransactionsResponse(accountTransactionsResponseMap);
+    coreBankingService.saveCoreBankingModel(coreBankingModel);
+    String response = "Re-initialised account response response successfully!";
+    return ResponseEntity.ok(response);
+  }
+
+  /**
+   * Reset Card related Transactions for specific customer
+   *
+   * @param cardTransactionsResponseMap
+   * @param customerId
+   * @param accountId
+   * @return
+   */
+  @ApiOperation(value = "Update Payee Response for specific users")
+  @RequestMapping(value = "/init/{customerId}/cards/transactions", consumes = {"application/json"}, method = RequestMethod.POST)
+  public ResponseEntity<String> resetCardTransactionsResponse(
+      @RequestBody Map<String, CardTransactionsResponse> cardTransactionsResponseMap,
+      @PathVariable(name = "customerId", required = true) String customerId,
+      @PathVariable(name = "accountId", required = true) String accountId) {
+    ApplicationLogger.logInfo("Updating card response response...");
+    CoreBankingModel coreBankingModel = coreBankingService.getCoreBankingModel(customerId);
+    coreBankingModel.setCardTransactionsResponse(cardTransactionsResponseMap);
+    coreBankingService.saveCoreBankingModel(coreBankingModel);
+    String response = "Re-initialised card response response successfully";
+    return ResponseEntity.ok(response);
   }
 
 }
