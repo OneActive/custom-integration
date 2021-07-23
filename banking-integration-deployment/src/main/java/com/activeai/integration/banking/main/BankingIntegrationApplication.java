84933de7ab3d06ecff900704af955ce534f1acdf
Manjunath <manjunath.h@active.ai>
2021-07-23 17:59:40 +0530

    Unlinked redis dependency

--- banking-integration-deployment/src/main/java/com/activeai/integration/banking/main/BankingIntegrationApplication.java
+++ banking-integration-deployment/src/main/java/com/activeai/integration/banking/main/BankingIntegrationApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.activeai.integration")
